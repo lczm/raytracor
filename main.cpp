6bab9c951d6dadfbc8f2d04af875e6b79470e2e4
lczm <zemingchua@gmail.com>
2021-02-25 18:18:31 +0800

    Pseudo progress bar

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\rScanlines Remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
@@ -21,6 +22,8 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::cerr << "\nDone.\n";
 
     return 0;
 }
