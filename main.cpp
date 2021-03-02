3c22a53d999cf950b7e337e89e9410387ee59916
lczm <zemingchua@gmail.com>
2021-03-03 02:23:50 +0800

    Change resolution

--- main.cpp
+++ main.cpp
@@ -57,7 +57,7 @@ color ray_color(const ray& r) {
 int main() {
     // Image dimensions, 16:9 aspect ratio, calculate width & height
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const int image_width = 1280;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     // Camera
