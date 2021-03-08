cb5e26a8c31ad8f1a0860f8f20f52028a56bac6a
lczm <zemingchua@gmail.com>
2021-03-08 13:53:17 +0800

    Gamma correct the image

--- color.h
+++ color.h
@@ -9,10 +9,13 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     auto b = pixel_color.z();
 
     // Divide the color by the number of samples
+    // and gamma-correct for gamma=2.0
+    // Gamma correct the images, image viewers expect this
+    // https://en.wikipedia.org/wiki/Gamma_correction
     auto scale = 1.0 / samples_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
 
     // Write the translated [0,255] value of each color component.
     out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
